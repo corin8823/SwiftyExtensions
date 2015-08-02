37e65e12746a74edef56a268ddf727745fd1990f
corin8823 <yusuke_takahashi@cyberagent.co.jp>
2015-08-02 13:49:32 +0900

    Initial Commit

---
+++ SwiftyExtensions/SwiftyExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyExtensions.h
+//  SwiftyExtensions
+//
+//  Created by 髙橋 優介 on 8/2/15.
+//  Copyright (c) 2015 yusuke takahashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyExtensions.
+FOUNDATION_EXPORT double SwiftyExtensionsVersionNumber;
+
+//! Project version string for SwiftyExtensions.
+FOUNDATION_EXPORT const unsigned char SwiftyExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyExtensions/PublicHeader.h>
+
+
